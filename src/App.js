89b3ff11899a939f5ff2ae06b1d77ec9d4a54860
Nestor_Damien <75420711+Nestor-19@users.noreply.github.com>
2024-04-17 17:48:36 -0400

    Revert "Created Navbar component and initialized firebase"
    This reverts commit 333c8f708499a4772073d7ef13357e890de0ab14.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import { Routes, Route} from "react-router-dom";
 import Home from "./routes/home/home";
-import Navbar from "./components/Navbar/Navbar";
 
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Navbar />}>
-        <Route index element={<Home />} />
-      </Route>
+        <Route path='/' index element={<Home />} />
     </Routes>
   );
 };
