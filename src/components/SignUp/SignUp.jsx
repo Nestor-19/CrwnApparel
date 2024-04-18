ef6ff7bd7fa6edd6b9eaff6a6a192b47a406e6a9
Nestor_Damien <75420711+Nestor-19@users.noreply.github.com>
2024-04-18 10:51:06 -0400

    Created SignUp component and added global styling

---
+++ src/components/SignUp/SignUp.jsx
@@ -0,0 +1,100 @@
+import { useState } from "react";
+import { createUserDocument, createUserWithEmailAndPass } from "../../utils/firebase/firebase";
+import './SignUp.styles.scss';
+import FormInput from "../FormInput/FormInput";
+import Button from "../Button/Button";
+
+const defaultFormFields = {
+    displayName: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+}
+
+const SignUpForm = () => {
+    const [formFields, setFormFields] = useState(defaultFormFields);
+    const {displayName, email, password, confirmPassword} = formFields;
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        if (password !== confirmPassword) {
+            alert('Passwords do not match');
+            return;
+        } 
+
+        try {
+            const response = await createUserWithEmailAndPass(email, password);
+            await createUserDocument(response.user, {displayName});
+            resetFormFields();
+        } catch (error) {
+            if (error.code === 'auth/email-already-in-use') {
+                alert('Cannot create user with existing email');
+            } else {
+                console.log('error creating user', error.message);
+            }
+        }
+    }
+
+    const resetFormFields = () => {
+        setFormFields(defaultFormFields);
+    }
+
+    const handleEvent = (event) => {
+        const {name, value} = event.target;
+        setFormFields({...formFields, [name]: value});
+    }
+
+    return (
+        <div className="sign-up-container">
+            <h2>Don't have an account?</h2>
+            <span>Sign up with your email and password</span>
+            <form onSubmit={handleSubmit}>
+                <FormInput 
+                    label="Display Name"
+                    inputOptions = {{
+                        type: "text",
+                        required: true,
+                        onChange: handleEvent,
+                        name: "displayName",
+                        value: displayName
+                    }}
+                />
+                <FormInput 
+                    label="Email"
+                    inputOptions = {{
+                        type: "email",
+                        required: true,
+                        onChange: handleEvent,
+                        name: "email",
+                        value: email
+                    }}
+                />
+                <FormInput 
+                    label="Password"
+                    inputOptions = {{
+                        type: "password",
+                        required: true,
+                        onChange: handleEvent,
+                        name: "password",
+                        value: password
+                    }}
+                />
+                <FormInput 
+                    label="Confirm Password"
+                    inputOptions = {{
+                        type: "password",
+                        required: true,
+                        onChange: handleEvent,
+                        name: "confirmPassword",
+                        value: confirmPassword
+                    }}
+                />
+
+                <Button type="submit">Sign Up</Button>
+            </form>
+        </div>
+    )
+}
+
+export default SignUpForm;
--- src/routes/authentication/authentication.jsx
+++ src/routes/authentication/authentication.jsx
@@ -1,10 +1,12 @@
 import SignInForm from '../../components/SignIn/SignIn';
+import SignUpForm from '../../components/SignUp/SignUp';
 import './authentication.styles.scss'
 
 const Authentication = () => {
     return (
         <div className="authentication-container">
             <SignInForm />
+            <SignUpForm />
         </div>
     )
 }
