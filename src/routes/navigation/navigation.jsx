fe719ca2e53f96fea47154c9b98f9dbdb33adc32
Nestor_Damien <75420711+Nestor-19@users.noreply.github.com>
2024-04-18 11:03:57 -0400

    Implemented User Context to keep track of authentication states

--- src/routes/navigation/navigation.jsx
+++ src/routes/navigation/navigation.jsx
@@ -1,30 +1,35 @@
+import React from "react";
+import { Outlet, Link } from "react-router-dom";
+import { Fragment, useContext } from "react";
+import {ReactComponent as CrownLogo} from '../../assets/crown.svg'
+import './navigation.styles.scss'
+import { UserContext } from "../../contexts/UserContext";
+import { signOutUser } from "../../utils/firebase/firebase";
 
-import { Fragment } from 'react';
-import { Outlet, Link } from 'react-router-dom';
-
-import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
-
-import './navigation.styles.scss';
-
-const Navigation = () => {
-  return (
+const NavBar = () => {
+    const { currentUser } = useContext(UserContext);
+    console.log(currentUser);
+    return (
     <Fragment>
-      <div className='navigation'>
-        <Link className='logo-container' to='/'>
-          <CrwnLogo className='logo' />
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+            <CrownLogo className="logo"/>
         </Link>
-        <div className='nav-links-container'>
-          <Link className='nav-link' to='/shop'>
-            SHOP
-          </Link>
-          <Link className='nav-link' to='/sign-in'>
-            SIGN IN
-          </Link>
+        <div className="nav-links-container">
+            <Link className="nav-link" to="/shop">
+                SHOP
+            </Link>
+            {currentUser
+                ? <span className="nav-link" onClick={signOutUser}>SIGN OUT</span>
+                : <Link className="nav-link" to="/auth">
+                    SIGN IN
+                  </Link>  
+            }
         </div>
       </div>
       <Outlet />
     </Fragment>
-  );
-};
+    )
+}
 
-export default Navigation;
+export default NavBar;
