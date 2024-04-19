3f0c2f327783560d7f7d2722605b2c06756b57bf
Nestor_Damien <75420711+Nestor-19@users.noreply.github.com>
2024-04-18 20:37:49 -0400

    Created Category Preview component to only show four products on the main shop page

--- src/routes/shop/shop.jsx
+++ src/routes/shop/shop.jsx
@@ -1,24 +1,18 @@
-import { useContext } from "react";
+import { useContext, Fragment } from "react";
 import { CategoriesContext } from "../../contexts/CategoriesContext";
-import ProductCard from "../../components/ProductCard/ProductCard";
+import CategoryPreview from "../../components/CategoryPreview/CategoryPreview";
 import './shop.styles.scss';
-import { Fragment } from "react/cjs/react.production.min";
 
 const Shop = () => {
     const {categoriesMap} = useContext(CategoriesContext);
     console.log(categoriesMap);
 
     return (
-        <div className="products-container">
-            {
-                Object.keys(categoriesMap).map((title) => (
-                    <Fragment key={title}>
-                        {categoriesMap[title].map((product) => (
-                            <ProductCard key={product.id} product={product}/>
-                        ))}
-                    </Fragment>
-                ))
-            }
+        <div className='shop-container'>
+            {Object.keys(categoriesMap).map((key) => {
+                const products = categoriesMap[key];
+                return <CategoryPreview key={key} title={key} products={products} />;
+            })}
         </div>
     )
 }
