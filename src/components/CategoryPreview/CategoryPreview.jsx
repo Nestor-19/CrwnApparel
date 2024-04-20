ccdc1caecf261284ddf693a9bba3530198b61657
Nestor_Damien <75420711+Nestor-19@users.noreply.github.com>
2024-04-20 11:11:45 -0400

    Added dynamic path for categories on shop page

--- src/components/CategoryPreview/CategoryPreview.jsx
+++ src/components/CategoryPreview/CategoryPreview.jsx
@@ -1,19 +1,25 @@
+import { Link } from 'react-router-dom';
 import ProductCard from '../ProductCard/ProductCard';
 import './CategoryPreview.styles.scss';
 
-const CategoryPreview = ({ title, products }) => (
-  <div className='category-preview-container'>
-    <h2>
-      <span className='title'>{title.toUpperCase()}</span>
-    </h2>
-    <div className='preview'>
-      {products
-        .filter((_, idx) => idx < 4)
-        .map((product) => (
-          <ProductCard key={product.id} product={product} />
-        ))}
+const CategoryPreview = ({ title, products }) => {
+  const path = `/shop/${title}`
+  return (
+    <div className='category-preview-container'>
+      <Link to={path}>
+        <h2>
+            <span className='title'>{title.toUpperCase()}</span>
+        </h2>
+      </Link>
+      <div className='preview'>
+        {products
+          .filter((_, idx) => idx < 4)
+          .map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default CategoryPreview;
