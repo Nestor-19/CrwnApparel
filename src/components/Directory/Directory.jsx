4a192353db9817a4851e99cff0d4d6ae2b9663c5
Nestor_Damien <75420711+Nestor-19@users.noreply.github.com>
2024-04-20 11:00:12 -0400

    Fixed scss styling issue and renamed CategoryItem component to DirectoryItem

--- src/components/Directory/Directory.jsx
+++ src/components/Directory/Directory.jsx
@@ -1,4 +1,4 @@
-import CategoryItem from '../CategoryItem/CategoryItem';
+import DirectoryItem from '../DirectoryItem/DirectoryItem';
 
 import './Directory.styles.scss'
 
@@ -6,7 +6,7 @@ const Directory = ({ categories }) => {
   return (
     <div className='directory-container'>
       {categories.map((category) => (
-        <CategoryItem key={category.id} category={category} />
+        <DirectoryItem key={category.id} category={category} />
       ))}
     </div>
   );
