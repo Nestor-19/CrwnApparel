c17035f8ce151527b2454ae8c0800549c27c3460
Nestor_Damien <75420711+Nestor-19@users.noreply.github.com>
2024-04-30 22:10:56 -0400

    Created intial files to migrate CartContext to cartReducer

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import { userReducer } from "./user/userReducer";
 import { categoriesReducer } from "./categories/categoriesReducer";
+import { CartReducer } from "./cart/cartReducer";
 
 export const rootReducer = combineReducers({
     user: userReducer,
-    categories: categoriesReducer
+    categories: categoriesReducer,
+    cart: CartReducer
 })
