0f0fecd29770d5faa4f5b7a625ff4351dc215a43
Nestor_Damien <75420711+Nestor-19@users.noreply.github.com>
2024-05-05 19:43:39 -0400

    Set up initial usage of redux-persist

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,15 @@
 import { configureStore, Tuple } from "@reduxjs/toolkit";
+import {persistStore, persistReducer} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import logger from 'redux-logger';
 import { rootReducer } from './rootReducer';
 
 const middlewares = [logger];
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
 
 export const store = configureStore({
   reducer: rootReducer,
