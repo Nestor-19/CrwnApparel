95763f5f35fab070155fffc45819f6c6a4ba9ff6
Nestor_Damien <75420711+Nestor-19@users.noreply.github.com>
2024-05-12 19:30:09 -0400

    Added functionality to make fetch request to create a payment and confirm the payment

--- src/components/PaymentForm/PaymentForm.jsx
+++ src/components/PaymentForm/PaymentForm.jsx
@@ -13,11 +13,37 @@ const PaymentForm = () => {
             return;
         }
 
+        const response = await fetch('/.netlify/functions/createPayment', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ amount: 10000 })
+        }).then(res => res.json());
+        
+        const clientSecret = response.paymentIntent.client_secret;
+
+        const paymentResult = await stripe.confirmCardPayment(clientSecret, {
+            payment_method: {
+                card: elements.getElement(CardElement),
+                billing_details: {
+                    name: 'Nestor Dsouza'
+                }
+            }
+        });
+
+        if (paymentResult.error) {
+            alert(paymentResult.error);
+        } else{
+            if (paymentResult.paymentIntent.status === 'succeeded') {
+                alert('Payment Successfull!');
+            }
+        }
     }
 
     return (
         <div className='payment-form-container'>
-            <form className='form-container'>
+            <form className='form-container' onSubmit={handlePayment}>
                 <h2>Credit Card Payment: </h2>
                 <CardElement />
                 <Button buttonType="inverted" className='payment-button'>Pay Now</Button>
