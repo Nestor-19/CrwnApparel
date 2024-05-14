d09b0d5abecd82928ed14b6e8719683c7d346656
Nestor_Damien <75420711+Nestor-19@users.noreply.github.com>
2024-05-14 19:27:28 -0400

    Added testing buttons with different buttonTypes and isLoading values

--- src/components/Button/__tests__/Button.test.js
+++ src/components/Button/__tests__/Button.test.js
@@ -3,8 +3,26 @@ import Button from "../Button";
 
 describe('Button Tests', () => {
     test('Should render default button when no text is passed', () => {
-        render(<Button></Button>);
+        render(<Button />);
         const buttonElement = screen.getByRole('button');
         expect(buttonElement).toHaveClass('undefined');
+    })
+
+    test('Should render google button when buttonType="google"', () => {
+        render(<Button buttonType="google" />);
+        const buttonElement = screen.getByRole('button');
+        expect(buttonElement).toHaveClass('google-sign-in');
+    })
+
+    test('Should render inverted button when buttonType="inverted"', () => {
+        render(<Button buttonType="inverted" />);
+        const buttonElement = screen.getByRole('button');
+        expect(buttonElement).toHaveClass('inverted');
+    })
+
+    test('Should disable button when isLoading=true', () => {
+        render(<Button isLoading={true} />);
+        const buttonElement = screen.getByRole('button');
+        expect(buttonElement).toBeDisabled();
     })
 })
