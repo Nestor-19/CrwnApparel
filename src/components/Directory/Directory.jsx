c4cdc66c237c540c8306a61628c91afd0c729622
Nestor_Damien <75420711+Nestor-19@users.noreply.github.com>
2024-05-23 20:49:26 -0400

    Updated images on home page in Directory component

--- src/components/Directory/Directory.jsx
+++ src/components/Directory/Directory.jsx
@@ -6,31 +6,31 @@ const categories = [
   {
     id: 1,
     title: 'hats',
-    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+    imageUrl: 'https://3.bp.blogspot.com/-n5xdaceZdHA/Tfb4NsyuZGI/AAAAAAAAABo/9HIbE74sLR4/s1600/snap+hats2.jpg',
     route: 'shop/hats'
   },
   {
     id: 2,
     title: 'jackets',
-    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+    imageUrl: 'https://image-cdn.hypb.st/https%3A%2F%2Fhypebeast.com%2Fimage%2F2017%2F10%2Fevent-recap-readymade-maxfield-la-pop-up-23.jpg?cbr=1&q=90',
     route: 'shop/jackets'
   },
   {
     id: 3,
     title: 'sneakers',
-    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+    imageUrl: 'https://weartesters.com/wp-content/uploads/2021/11/IMG_8146-1024x768.jpg',
     route: 'shop/sneakers'
   },
   {
     id: 4,
     title: 'womens',
-    imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+    imageUrl: 'https://www.shutterstock.com/image-photo/beauty-styled-portrait-young-black-600nw-2110212626.jpg',
     route: 'shop/womens'
   },
   {
     id: 5,
     title: 'mens',
-    imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+    imageUrl: 'https://www.thefashionisto.com/wp-content/uploads/2018/11/Express-Men-Fall-2018-009-750x420.jpg',
     route: 'shop/mens'
   },
 ];
